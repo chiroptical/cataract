87eee33f1173d973df5c5a84bd92101eaa921873
Barry Moore <chiroptical@gmail.com>
2022-04-01 09:29:28 -0400

    Add basic animatorium express application

---
+++ animatorium/public/javascripts/basic.js
@@ -0,0 +1,4 @@
+SVG.on(document, 'DOMContentLoaded', function() {
+  var draw = SVG().addTo('body').size(1920, 1080);
+  var rect = draw.rect(200, 100).attr({ fill: '#975df9' });
+})
--- shell.nix
+++ shell.nix
@@ -3,16 +3,21 @@ pkgs.mkShell {
   inputsFrom = [
     (import ./pluto.nix pkgs).env
   ];
-  buildInputs = [
-    pkgs.haskell-language-server
-    pkgs.haskellPackages.cabal-install
-    pkgs.haskellPackages.ghcid
-    pkgs.haskellPackages.stylish-haskell
-    pkgs.haskellPackages.hlint
-    pkgs.haskellPackages.hpack
-    pkgs.haskellPackages.retrie
-    pkgs.niv
-    pkgs.alejandra
+  buildInputs = with pkgs; [
+    haskell-language-server
+    haskellPackages.cabal-install
+    haskellPackages.ghcid
+    haskellPackages.stylish-haskell
+    haskellPackages.hlint
+    haskellPackages.hpack
+    haskellPackages.retrie
+    niv
+    alejandra
+
+    # animatorium
+    nodejs
+    nodePackages.npm
+    nodePackages.nodemon
   ];
   withHoogle = true;
 }
