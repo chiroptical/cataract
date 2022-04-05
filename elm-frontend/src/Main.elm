13f7ff7a094823cf1a4f66d7f807aa7491a2c16e
Barry Moore <chiroptical@gmail.com>
2022-04-04 20:46:45 -0400

    Got layout unsure how to do animations though

--- elm-frontend/src/Main.elm
+++ elm-frontend/src/Main.elm
@@ -1,116 +1,96 @@
 module Main exposing (main)
 
-import Browser exposing (Document)
-import Browser.Navigation exposing (Key)
-import Html as H
-import Html.Attributes as A
-import Route exposing (Route)
-import Router exposing (Config, Router)
-import Url exposing (Url)
+-- import Html.Styled.Events exposing (onClick)
+-- import Svg.Styled as StyledSvg
+-- import Svg.Styled.Attributes as SvgAttribs
+
+import Browser
+import Browser.Dom as Dom
+import Css exposing (..)
+import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (css)
+import Svg.Snail exposing (snail)
+import Tailwind.Utilities as Tw
+import Task
 
 
-
--- MODEL
-
-
-type alias Model =
-    { router : Router Route }
-
-
-
--- MSG
-
-
-type Msg
-    = Router (Router.Msg Route.Msg)
-    | UrlChanged Url
-
-
-
--- ROUTER CONFIG
-
-
-config : Config Msg Route Route.Msg
-config =
-    { parser = Route.parser
-    , update = Route.update
-    , view = Route.view
-    , message = Router
-    , subscriptions = Route.subscriptions
-    , notFound = Route.notFound
-    , routeTitle = Route.title
-    , onUrlChanged = Just UrlChanged
-    }
-
-
-
--- INIT
-
-
-init : () -> Url -> Key -> ( Model, Cmd Msg )
-init _ url key =
-    let
-        ( router, cmd ) =
-            Router.init config url key
-    in
-    ( Model router, cmd )
-
-
-
--- UPDATE
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update message ({ router } as model) =
-    case message of
-        Router msg ->
-            let
-                ( newRouter, cmd ) =
-                    Router.update config msg router
-            in
-            ( { model | router = newRouter }, cmd )
-
-        UrlChanged _ ->
-            ( model, Cmd.none )
-
-
-
--- VIEW
-
-
-view : Model -> Document Msg
-view { router } =
-    { title = Router.title router "Pluto"
-    , body =
-        [ H.nav []
-            [ H.a [ A.href "/" ] [ H.text "Home" ]
-            , H.a [ A.href "/overlay" ] [ H.text "Overlay" ]
+view : Model -> Html Msg
+view _ =
+    div
+        [ css
+            [ width (px 1920)
+            , height (px 1080)
             ]
-        , H.main_ [] (Router.view config router)
         ]
-    }
-
-
-
--- SUBSCRIPTIONS
-
-
-subscriptions : Model -> Sub Msg
-subscriptions { router } =
-    Router.subscriptions config router
-
-
-
--- MAIN
+        [ div
+            [ css
+                [ Tw.text_7xl
+                , Tw.text_purple_900
+                , position absolute
+                , top (px 1800)
+                , left (px 800)
+                ]
+            ]
+            [ text "Followers 12" ]
+        , div
+            [ css
+                [ Tw.text_7xl
+                , Tw.text_purple_900
+                , position absolute
+                , top (px 1400)
+                , left (px 400)
+                ]
+            ]
+            [ text "Subscriber 1234" ]
+        , div
+            [ css
+                [ position absolute
+                , top (px 1000)
+                , left (px 200)
+                , transform (scale 0.5)
+                ]
+            ]
+            [ snail ]
+        ]
 
 
 main : Program () Model Msg
 main =
-    Browser.application
-        { init = init
+    Browser.element
+        { view = view >> toUnstyled
         , update = update
-        , view = view
+        , init = initialModel
         , subscriptions = subscriptions
-        , onUrlChange = Router.onUrlChange config
-        , onUrlRequest = Router.onUrlRequest config
         }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update _ model =
+    ( model, Cmd.none )
+
+
+type Msg
+    = NoOp
+
+
+setViewport : Cmd Msg
+setViewport =
+    Task.perform (\_ -> NoOp) (Dom.setViewport 1920 1080)
+
+
+type alias Model =
+    ()
+
+
+
+-- First argument is flags
+
+
+initialModel : () -> ( Model, Cmd Msg )
+initialModel _ =
+    ( (), setViewport )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
