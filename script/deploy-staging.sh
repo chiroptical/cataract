d2ba7f686a7e535252c522e0f7035b236ff9a0ad
Barry <chiroptical@gmail.com>
2022-04-19 10:06:06 -0400

    Moving back to Heroku, lol

--- script/deploy-staging.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-imageId=$(docker images --format "{{.Repository}} {{.ID}} {{.CreatedAt}}" | grep "^cataract" | head -1 | awk '{print $2}')
-echo "Pushing image with id: $imageId"
-docker tag $imageId registry.heroku.com/chiroptical-cataract/web
-docker push registry.heroku.com/chiroptical-cataract/web
-heroku container:release -a chiroptical-cataract web
--- script/deploy.sh
+++ script/deploy.sh
@@ -2,5 +2,6 @@
 
 imageId=$(docker images --format "{{.Repository}} {{.ID}} {{.CreatedAt}}" | grep "^cataract" | head -1 | awk '{print $2}')
 echo "Pushing image with id: $imageId"
-docker tag $imageId registry.digitalocean.com/chiroptical/cataract
-docker push registry.digitalocean.com/chiroptical/cataract
+docker tag $imageId registry.heroku.com/cataract-chiroptical/web
+docker push registry.heroku.com/cataract-chiroptical/web
+heroku container:release -a cataract-chiroptical web
