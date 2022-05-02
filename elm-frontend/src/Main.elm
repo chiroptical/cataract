ffed79eebfa3fff429da9aeafa7c7404a242cc0e
Barry Moore <chiroptical@gmail.com>
2022-05-02 09:27:16 -0400

    Update elm app

--- elm-frontend/src/Main.elm
+++ elm-frontend/src/Main.elm
@@ -6,6 +6,9 @@ module Main exposing (main)
 
 import Browser
 import Browser.Dom as Dom
+import Canvas exposing (..)
+import Canvas.Settings as Canvas exposing (..)
+import Color
 import Css exposing (..)
 import Html.Styled exposing (..)
 import Html.Styled.Attributes exposing (css)
@@ -16,42 +19,60 @@ import Task
 
 view : Model -> Html Msg
 view _ =
-    div
-        [ css
-            [ width (px 1920)
-            , height (px 1080)
-            ]
+    let
+        width =
+            1920
+
+        height =
+            1080
+    in
+    Canvas.toHtml ( width, height )
+        []
+        [ shapes [ Canvas.fill Color.white ] [ rect ( 0, 0 ) 50 50 ]
+        , shapes [ Canvas.fill Color.red ] [ rect ( 50, 50 ) 100 100 ]
         ]
-        [ div
-            [ css
-                [ Tw.text_7xl
-                , Tw.text_purple_900
-                , position absolute
-                , top (px 1800)
-                , left (px 800)
-                ]
-            ]
-            [ text "Followers 12" ]
-        , div
-            [ css
-                [ Tw.text_7xl
-                , Tw.text_purple_900
-                , position absolute
-                , top (px 1400)
-                , left (px 400)
-                ]
-            ]
-            [ text "Subscriber 1234" ]
-        , div
-            [ css
-                [ position absolute
-                , top (px 1000)
-                , left (px 200)
-                , transform (scale 0.5)
-                ]
-            ]
-            [ snail ]
-        ]
+        |> fromUnstyled
+
+
+
+-- view : Model -> Html Msg
+-- view _ =
+--     div
+--         [ css
+--             [ width (px 1920)
+--             , height (px 1080)
+--             ]
+--         ]
+--         [ div
+--             [ css
+--                 [ Tw.text_7xl
+--                 , Tw.text_purple_900
+--                 , position absolute
+--                 , top (px 1800)
+--                 , left (px 800)
+--                 ]
+--             ]
+--             [ text "Followers 12" ]
+--         , div
+--             [ css
+--                 [ Tw.text_7xl
+--                 , Tw.text_purple_900
+--                 , position absolute
+--                 , top (px 1400)
+--                 , left (px 400)
+--                 ]
+--             ]
+--             [ text "Subscriber 1234" ]
+--         , div
+--             [ css
+--                 [ position absolute
+--                 , top (px 1000)
+--                 , left (px 200)
+--                 , transform (scale 0.5)
+--                 ]
+--             ]
+--             [ snail ]
+--         ]
 
 
 main : Program () Model Msg
