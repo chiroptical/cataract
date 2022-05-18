0d1405e023d3c0bdae8bf64d6f00358a0cfec7ea
Barry Moore <chiroptical@gmail.com>
2022-05-18 09:15:09 -0400

    Doesn't appear to work

--- elm-app/src/Page/Home.elm
+++ elm-app/src/Page/Home.elm
@@ -9,15 +9,85 @@ module Page.Home exposing
 
 import Css exposing (..)
 import Html.Styled exposing (..)
+import Html.Styled.Attributes as Attributes exposing (css)
 import Platform.Sub as Sub
+import Route
 import Session exposing (Session)
+import Tailwind.Utilities as Tw
 
 
 view : Model -> Html Msg
 view _ =
-    div
-        []
-        [ text "Hello Home"
+    -- <nav class="bg-white shadow">
+    nav
+        [ css
+            [ Tw.bg_white
+            , Tw.shadow
+            ]
+        ]
+        -- <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        [ div
+            [ css
+                [ Tw.max_w_7xl
+                , Tw.mx_auto
+                , Tw.px_4
+                , Tw.px_6
+                , Tw.px_8
+                ]
+            ]
+            -- <div class="flex justify-between h-16">
+            [ div
+                [ css
+                    [ Tw.flex
+                    , Tw.justify_between
+                    , Tw.h_16
+                    ]
+                ]
+                -- <div class="flex">
+                [ div
+                    [ css [ Tw.flex ] ]
+                    [ div
+                        [ css
+                            [ Tw.hidden
+                            , Tw.ml_6
+                            , Tw.flex
+                            , Tw.space_x_8
+                            ]
+                        ]
+                        [ a
+                            [ css
+                                [ Tw.border_indigo_500
+                                , Tw.text_gray_900
+                                , Tw.inline_flex
+                                , Tw.items_center
+                                , Tw.px_1
+                                , Tw.pt_1
+                                , Tw.border_b_2
+                                , Tw.text_sm
+                                , Tw.font_medium
+                                ]
+                            , Route.href Route.Home
+                            ]
+                            [ text "Home" ]
+                        , a
+                            [ css
+                                [ Tw.border_indigo_500
+                                , Tw.text_gray_900
+                                , Tw.inline_flex
+                                , Tw.items_center
+                                , Tw.px_1
+                                , Tw.pt_1
+                                , Tw.border_b_2
+                                , Tw.text_sm
+                                , Tw.font_medium
+                                ]
+                            , Route.href Route.Overlay
+                            ]
+                            [ text "Overlay" ]
+                        ]
+                    ]
+                ]
+            ]
         ]
 
 
